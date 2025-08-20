9b8f9452c7ed41a5e03e5c5e3155cea594e361d6
sauldavilat <saul.metropoli@gmail.com>
2025-08-19 20:37:16 -0600

    Incluyendo build compilado para producción

---
+++ public/build/assets/InputError-BipjQy2Z.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-DM6KxBPp.js";function s({message:t,className:r="",...e}){return t?n.jsx("p",{...e,className:"text-sm text-red-600 "+r,children:t}):null}export{s as default};
